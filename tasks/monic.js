72b6d705840adf0def572b7d82c836a7f708f952
kobezzza <kobezzza@gmail.com>
2015-05-03 12:31:31 +0300

    Refactoring

--- tasks/monic.js
+++ tasks/monic.js
@@ -6,59 +6,72 @@
  * https://github.com/MonicBuilder/grunt-monic/blob/master/LICENSE
  */
 
-var monic = require('monic');
+var
+	$C = require('collection.js').$C;
+
+var
+	monic = require('monic'),
+	async = require('async'),
+	path = require('path');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('monic', 'Using Monic', function () {
 		var
 			done = this.async(),
-			that = this;
+			opts = $C.extend(true, {}, this.options(), {saveFiles: true, cwd: process.cwd()}),
+			tasks = [];
 
-		var
-			i = 0,
-			res = '';
-
-		this.files.forEach(function (file) {
-			file.src.filter(function (src) {
-				if (grunt.file.exists(src)) {
-					return true;
-				}
-
-				grunt.log.warn('Source file "' + src + '" not found.');
-				return false;
-
-			}).forEach(function (src) {
-				try {
-					i++;
-
-					var opts = that.options();
-
-					opts.content = grunt.file.read(src);
-					opts.saveFiles = false;
-
-					if (opts.sourceMaps) {
-						opts.file = opts.file || file.dest;
+		$C(this.files).forEach(function (file) {
+			var baseSrc = null;
+			var content = $C(file.src).reduce(
+				function (res, src) {
+					if (baseSrc === null) {
+						baseSrc = src;
 					}
 
-					monic.compile(src, opts, function (err, data) {
+					return res += grunt.file.read(src);
+				},
+
+				'',
+
+				{
+					filter: function (src) {
+						if (grunt.file.exists(src)) {
+							return true;
+						}
+
+						grunt.log.warn('Source file "' + src + '" not found.');
+						return false;
+					}
+				}
+			);
+
+			tasks.push(function (cb) {
+				var params = $C.extend(true, {}, opts, {content: content});
+				params.file = params.file || file.dest;
+
+				try {
+					monic.compile(baseSrc, params, function (err) {
 						if (err) {
-							return grunt.log.error(err.message);
+							return cb(err);
 						}
 
-						i--;
-						res += data;
-
-						if (!i) {
-							grunt.file.write(file.dest, res);
-							grunt.log.writeln('File "' + file.dest + '" created.');
-							done(res);
-						}
+						grunt.log.writeln('File "' + params.file + '" created.');
 					});
 
 				} catch (err) {
-					grunt.log.error(err.message);
+					cb(err);
 				}
 			});
 		});
+
+		async.parallel(tasks, function (err) {
+			if (err) {
+				return grunt.log.error(err.message);
+			}
+
+			done();
+		});
 	});
 };
+
